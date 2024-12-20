6dc7abc7ae5d7bd6e809a7b6deaf8b7a4f6341de
EliazarNoa <193003@unsaac.edu.pe>
2024-12-20 18:03:32 -0500

    Subiendo el proyecto

---
+++ Moviles2024-2-G2/AsistenciaUPeUJCN/app/src/main/java/pe/edu/upeu/asistenciaupeujcn/utils/TokenUtils.kt
@@ -0,0 +1,11 @@
+package pe.edu.upeu.asistenciaupeujcn.utils
+
+import android.content.Context
+
+object TokenUtils {
+    var TOKEN_CONTENT="Aqui va el Token"
+    var API_URL = "http://192.168.0.13:8080/" // 10.0.2.2:8080
+    lateinit var CONTEXTO_APPX: Context
+    var USER_LOGIN=""
+    var ID_ASIS_ACT=0L
+}
